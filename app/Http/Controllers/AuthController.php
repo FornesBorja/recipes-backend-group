3bc131e581e7de9a10280189b4b21a9ac0ccb2c8
“FornesBorja” <fornes.borja@gmail.com>
2024-09-13 17:55:15 +0200

    feat: now when showing the user, password is hidden

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -15,70 +15,83 @@ use Symfony\Component\HttpFoundation\Response;
 class AuthController extends Controller
 {
     public function register(Request $request)
-{
-    $validator = Validator::make($request->all(), [
-        'name' => 'required|string|max:255',
-        'email' => 'required|string|email|max:255|unique:users',
-        'password' => 'required|string|min:8|confirmed',
-    ]);
-    if ($validator->fails()) {
-        return response()->json([
-            'success' => false,
-            'errors' => $validator->errors()
-        ], Response::HTTP_UNPROCESSABLE_ENTITY);
-    }
-    $user = User::create([
-        'name' => $request->name,
-        'email' => $request->email,
-        'password' => Hash::make($request->password),
-    ]);
-    return response()->json([
-        'success' => true,
-        'message' => 'User registered',
-        'user' => $user,
-    ], Response::HTTP_OK);
-}
-
-public function login(Request $request)
-{
-    // Validar los campos del request
-    $request->validate([
-        'email' => 'required|string',
-        'password' => 'required|string',
-    ]);
-
-    // Buscar el usuario por su email
-    $user = User::where('email', $request['email'])->first();
-
-    // Validar si el usuario existe
-    if (!$user) {
-        return response()->json([
-            'success' => false,
-            'message' => 'Email or password are invalid',
-        ], Response::HTTP_NOT_FOUND);
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+        if ($validator->fails()) {
+            return response()->json(
+                [
+                    'success' => false,
+                    'errors' => $validator->errors(),
+                ],
+                Response::HTTP_UNPROCESSABLE_ENTITY
+            );
+        }
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        $user->makeHidden(['password']);
+        return response()->json(
+            [
+                'success' => true,
+                'message' => 'User registered',
+                'user' => $user,
+            ],
+            Response::HTTP_OK
+        );
     }
 
-    // Validar la contraseña
-    if (!Hash::check($request['password'], $user->password)) {
-        return response()->json([
-            'success' => false,
-            'message' => 'Email or password are invalid',
-        ], Response::HTTP_NOT_FOUND);
-    }
+    public function login(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|string',
+            'password' => 'required|string',
+        ]);
 
-    // Generar el token JWT
-    if (!$token = JWTAuth::fromUser($user)) {
-        return response()->json([
-            'success' => false,
-            'message' => 'Could not create token',
-        ], Response::HTTP_INTERNAL_SERVER_ERROR);
-    }
+        $user = User::where('email', $request['email'])->first();
 
-    // Retornar el token junto con los datos del usuario
-    return response()->json([
-        'success' => true,
-        'token' => $token,
-        'user' => $user
-    ], Response::HTTP_ACCEPTED);
-}
+        if (!$user) {
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => 'Email or password are invalid',
+                ],
+                Response::HTTP_NOT_FOUND
+            );
+        }
+
+        if (!Hash::check($request['password'], $user->password)) {
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => 'Email or password are invalid',
+                ],
+                Response::HTTP_NOT_FOUND
+            );
+        }
+
+        if (!($token = JWTAuth::fromUser($user))) {
+            return response()->json(
+                [
+                    'success' => false,
+                    'message' => 'Could not create token',
+                ],
+                Response::HTTP_INTERNAL_SERVER_ERROR
+            );
+        }
+        $user->makeHidden(['password']);
+        return response()->json(
+            [
+                'success' => true,
+                'token' => $token,
+                'user' => $user,
+            ],
+            Response::HTTP_ACCEPTED
+        );
+    }
 }
